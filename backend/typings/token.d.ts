ff0d248c8899a1ab94082d4a0b843f15b74697fd
wwalpha <wwalpha@gmail.com>
2021-03-07 02:15:23 +0000

    update definitions

---
+++ backend/typings/token.d.ts
@@ -0,0 +1,29 @@
+export type CognitoToken = {
+  sub: string;
+  aud: string;
+  email_verified: boolean;
+  token_use: string;
+  auth_time: number;
+  iss: string;
+  'cognito:username': string;
+  exp: number;
+  given_name: string;
+  iat: number;
+  email: string;
+  'custom:tenant_id': string;
+  'custom:role': string;
+};
+
+export interface UserPoolDetails {
+  userPoolId: string;
+  userPoolClientId: string;
+  identityPoolId: string;
+}
+
+/** get credentials from user token request */
+export interface UserTokenRequest {
+  token: string;
+}
+
+/** get credentials from user token response */
+export interface UserTokenResponse {}
