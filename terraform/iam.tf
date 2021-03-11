21973affcff7125631977364b34c8994cc8330fb
wwalpha <wwalpha@gmail.com>
2021-03-11 08:30:40 +0000

    add task policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,6 +10,30 @@ resource "aws_iam_role" "ecs_task" {
 }
 
 # ----------------------------------------------------------------------------------------------
+# AWS ECS Task Role Policy - CloudWatch Full Access
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "ecs_task_cloudwatch" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS ECS Task Role Policy - XRay Daemon WriteAccess
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "ecs_task_xray" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS ECS Task Role Policy - App Mesh Envoy Access
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "ecs_task_envoy" {
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess"
+}
+
+# ----------------------------------------------------------------------------------------------
 # AWS ECS Task Execution Role
 # ----------------------------------------------------------------------------------------------
 resource "aws_iam_role" "ecs_task_exec" {
@@ -18,6 +42,22 @@ resource "aws_iam_role" "ecs_task_exec" {
   lifecycle {
     create_before_destroy = false
   }
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS ECS Task Execution Policy - ECS Task Execution Policy
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_default" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ----------------------------------------------------------------------------------------------
+# AWS ECS Task Execution Policy - CloudWatch Full Access
+# ----------------------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_cloudwatch" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
 # ----------------------------------------------------------------------------------------------
